78ce07bc80d119d7ee8cdf69fe6c96301fd4ea4e
wildonion <marefimoghaddam@unixerr.com>
2022-03-13 21:40:22 +0330

    remove comment

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,6 @@
 
 
 
-/*
-        SAMPLE OUTPUT
-        ----------------
-        chunk 0 -> 0
-        chunk 1 -> 60
-                sending updated chunk 0 -channel-> ...
-        chunk 2 -> 210
-        chunk 3 -> 15
-                sending updated chunk 1 -channel-> ...
-                sending updated chunk 2 -channel-> ...
-                sending updated chunk 3 -channel-> ...
-        the result is 3985935
-*/
-
-
-
-
 use utils;
 
 
