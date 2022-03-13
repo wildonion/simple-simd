9ead2d2e480712a68aa81118b28eff1cba7182b3
wildonion <marefimoghaddam@unixerr.com>
2022-03-13 21:42:25 +0330

    change name to heavy_func

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,14 @@ fn main() {
     
     
     
-    let multiple = |chunk: u8| {
+    let heavy_func = |chunk: u8| {
         println!(":) Doing some heavy operation on chunk .... {:?}", chunk);
         chunk
     };
 
     
 
-    let result = utils::simd(3985935, multiple);
+    let result = utils::simd(3985935, heavy_func);
     println!("the result is {:?}", result);
 
 
